bd0aba1174d4c7911cadf11cb18e41dd84f17020
Vegadrone <spenaantonino@gmail.com>
2022-09-13 17:47:37 +0200

    scaffolding and blades creation

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Archive')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                ciaone
+            </div>
+        </div>
+    </div>
+@endsection
