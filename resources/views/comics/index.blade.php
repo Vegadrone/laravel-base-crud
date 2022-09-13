17eacd01728434ee774ae8572cc728a691ed6a7b
Vegadrone <spenaantonino@gmail.com>
2022-09-13 19:39:18 +0200

    print in table db infos

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,10 +3,40 @@
 @section('title', 'Archive')
 
 @section('main-content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-3">
-                ciaone
+            <div class="col">
+                <table class="table table-dark table-hover">
+                    <thead>
+                        <th>ID</th>
+                        <th colspan="2">Title</th>
+                        <th>Description</th>
+                        <th>Thumbnail</th>
+                        <th>Price</th>
+                        <th>Series</th>
+                        <th>Sale Date</th>
+                        <th>Type</th>
+                    </thead>
+                    <tbody>
+                        @forelse ($comics as $comic)
+                            <tr>
+                                <td>{{ $comic->id}}</td>
+                                <td colspan="2">{{ $comic->title }}</td>
+                                <td>{{ $comic->description}}</td>
+                                <td>{{ $comic->thumbnail }}</td>
+                                <td>{{ $comic->price}}</td>
+                                <td>{{ $comic->series}}</td>
+                                <td>{{ $comic->sale_date}}</td>
+                                <td>{{ $comic->type}}</td>
+                            </tr>
+                        @empty
+                            <tr colspan=10>
+                                <td>Non sono stati trovati fumetti da visualizzare</td>
+                            </tr>
+                        @endforelse
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
