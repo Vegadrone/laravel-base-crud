7518a7a52a96af68dbacb20051f3871a65d76906
Vegadrone <spenaantonino@gmail.com>
2022-09-16 21:50:46 +0200

    add a funny 404 error page, delete footer blade

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', '404 | QUI NON CI STA NULLA VA VIA!')
+
+@section('main-content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <img class="text-center" src="https://www.meme-arsenal.com/memes/c9e6371faa3b57eaee1d35595ca8e910.jpg" alt="not found">
+        </div>
+    </div>
+</div>
+@endsection
